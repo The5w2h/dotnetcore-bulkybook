7ff14a6dca45053c07aa90e40dcf0064e9439d1c
The5w2h <srivastava.tanuj@outlook.com>
2023-04-02 23:18:51 +0530

    Started implementing Repository Pattern

---
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IRepository<T> where T : class // IRepository is a GENERIC.
+    {
+        // To start, assume T = Category
+        IEnumerable<T> GetAll(); // T can be any class
+    }
+}
+
