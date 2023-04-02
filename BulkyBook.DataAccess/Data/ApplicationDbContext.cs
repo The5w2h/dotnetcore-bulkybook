a78708f83790a3b67ffd868b2e53d0e6371b2c6a
The5w2h <srivastava.tanuj@outlook.com>
2023-04-02 21:05:36 +0530

    Refactored the code to include daa access, models and utility class libs

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -1,17 +1,16 @@
 ﻿using System;
-using BulkyBookWeb.Models;
 using Microsoft.EntityFrameworkCore;
+using BulkyBook.Models;
 
-namespace BulkyBookWeb.Data
+namespace BulkyBook.DataAccess;
+
+public class ApplicationDbContext : DbContext
 {
-	public class ApplicationDbContext : DbContext
+	public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
 	{
-		public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
-		{
 
-		}
+	}
 
         public DbSet<Category> Categories { get; set; }
-	}
 }
 
--- BulkyBook.Models/Category.cs
+++ BulkyBook.Models/Category.cs
@@ -2,29 +2,28 @@
 using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 
-namespace BulkyBookWeb.Models
+namespace BulkyBook.Models;
+
+public class Category
 {
-	public class Category
+	public Category()
 	{
-		public Category()
-		{
-		}
-
-		//*4 columns* should be created in the Table in the Db
-
-		[Key] // makes the prop a *PRIMARY KEY* and IDENTITY col
-		public int Id { get; set; }
-
-		[Required]
-		public string Name { get; set; }
-
-		[DisplayName("Display Order")]
-		[Range(1,100,ErrorMessage = "The range must be between 1 and 100")]
-		public int DisplayOrder { get; set; }
-
-		public DateTime CreatedDateTime { get; set; } = DateTime.Now;
-		// a) To set default value, b) when an obj of this class is created, this is set as the default value
-
 	}
+
+	//*4 columns* should be created in the Table in the Db
+
+	[Key] // makes the prop a *PRIMARY KEY* and IDENTITY col
+	public int Id { get; set; }
+
+	[Required]
+	public string Name { get; set; } = "unknown";
+
+	[DisplayName("Display Order")]
+	[Range(1,100,ErrorMessage = "The range must be between 1 and 100")]
+	public int DisplayOrder { get; set; }
+
+	public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+	// a) To set default value, b) when an obj of this class is created, this is set as the default value
+
 }
 
