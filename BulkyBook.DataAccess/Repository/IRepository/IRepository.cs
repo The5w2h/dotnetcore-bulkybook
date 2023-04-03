22898073fcd6a4be16e2c35fdbf894ca65d17d73
The5w2h <srivastava.tanuj@outlook.com>
2023-04-04 00:02:13 +0530

    Implemented IRepository. Used <T> generics. IQueryable

--- BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
+++ BulkyBook.DataAccess/Repository/IRepository/IRepository.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using System.Linq.Expressions;
+
 namespace BulkyBook.DataAccess.Repository.IRepository
 {
     public interface IRepository<T> where T : class // IRepository is a GENERIC.
     {
         // To start, assume T = Category
+        T GetFirstOrDefault(Expression<Func<T, bool>> filter);
+
         IEnumerable<T> GetAll(); // T can be any class
+
+        void Add(T entity);
+
+        void Remove(T entity);
+
+        void RemoveRange(IEnumerable<T> entity);
     }
 }
 
