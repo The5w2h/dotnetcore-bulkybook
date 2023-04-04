fe6cb906aaac0ebfffd9d66c6f91e86246cb7c4a
The5w2h <srivastava.tanuj@outlook.com>
2023-04-04 23:03:58 +0530

    Update Data Access and Models

---
+++ BulkyBook.DataAccess/Repository/IRepository/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace BulkyBook.DataAccess.Repository.IRepository
+{
+    public interface IUnitOfWork
+    {
+        public ICategoryRepository Category { get; } // QUESTION: Why only get is used here?
+
+        public ICoverTypeRepository CoverType { get; }
+
+        void Save(); // Global methods are put in UnitOfWork
+    }
+}
+
---
+++ BulkyBook.Models/CoverType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBook.Models
+{
+    public class CoverType
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name="Cover Type")]
+        [Required]
+        public string Name { get; set; }
+    }
+}
+
